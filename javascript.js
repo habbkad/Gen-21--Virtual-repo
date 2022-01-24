75d76aabe05534668c9941185d917de43b43218e
habib <habibkadiri52@gmail.com>
2022-01-24 19:11:51 +0000

    added somthing

---
+++ javascript.js
@@ -0,0 +1,26 @@
+
+var nameUser = "Ama"
+
+var age = 1000
+
+var good = true
+
+/*const ghRate = 6.9
+
+let title_Of_Student = "Abu"
+name="Ama Kofi"
+
+const car ="benz"
+
+//car ="toyota"*/
+
+let noBoys="there are 50 boys in the class"
+let noGirls = "there are 50 girls in the class "
+
+//console.log(noBoys++)
+
+
+// console.log(noBoys--)
+
+document.write(noBoys +" and also <br>" + noGirls)
+
